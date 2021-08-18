5f9cbc5b2acfa82342bf2d8ade97338eb165895e
BarrelBrenner <rondolph@aol.com>
2021-08-18 18:55:11 -0500

    adjusted schema

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const fitnessSchema = new Schema(
+const workoutSchema = new Schema(
   {
     day: {
       type: Date,
@@ -50,12 +50,12 @@ const fitnessSchema = new Schema(
   }
 );
 
-fitnessSchema.virtual("totalDuration").get(function () {
+workoutSchema.virtual("totalDuration").get(function () {
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
 });
 
-const Workout = mongoose.model("Workout", fitnessSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
